e700adc6798ff57b247c01d9346852ff6c2511b6
Nilay-Shahane <nilayshahane@gmail.com>
2025-12-09 22:09:52 +0530

    db and requirement updates

--- db.py
+++ db.py
@@ -6,14 +6,44 @@ MONGO_URL = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["finguru"]
 transactions = db["transactions"]
+notifications = db["notification"]
 
 
 async def save_tx(transaction):
     clean = transaction.strip()
 
+    # Remove parentheses wrapper
     if clean.startswith("(") and clean.endswith(")"):
         clean = clean[1:-1]
-    
+
+    # Parse JSON
     transaction = json.loads(clean)
+
+    # Insert into DB
     result = await transactions.insert_one(transaction)
+    print(result)
     print("Inserted ID:", result.inserted_id)
+
+    # -----------------------
+    # Build Notification
+    # -----------------------
+    amount_rupees = transaction["amountPaise"] / 100
+    msg_content = transaction['category']
+
+    if transaction["type"] == "income":
+        msg_head = f"+₹{amount_rupees}"
+    else:
+        msg_head = f"-₹{amount_rupees}"
+
+    notif_doc = {
+        "msg_type": "transaction",
+        "msg_head": msg_head,
+        "msg_content": msg_content,
+        "userId": transaction.get("userId"),
+        "transactionId": str(result.inserted_id)
+    }
+
+    notif_result = await notifications.insert_one(notif_doc)
+    print("Notification inserted:", notif_result.inserted_id)
+
+    return {"transaction_id": str(result.inserted_id)}
